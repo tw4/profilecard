d40581d8e941cea85b59b0a3390225d4bedb74f2
mertsabinov <88425310+mertsabinov@users.noreply.github.com>
2022-12-29 16:50:01 +0300

    update: global font

--- src/components/globalStyles.ts
+++ src/components/globalStyles.ts
@@ -2,10 +2,10 @@ import { globalCss } from "@stitches/react";
 
 const globalStyles = globalCss({
   "@import": [
-    "https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap",
+    "https://fonts.googleapis.com/css2?family=Open+Sans&display=swap",
   ],
   "*": { margin: 0, padding: 0, boxSizing: "border-box" },
-  body: { fontFamily: "Roboto, sans-serif" },
+  body: { fontFamily: "Open Sans, sans-serif" },
 });
 
 export default globalStyles;
