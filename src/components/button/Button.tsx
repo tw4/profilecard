678dfe4a53638713d988f75baaa920cbea62b45e
mertsabinov <88425310+mertsabinov@users.noreply.github.com>
2022-12-29 12:58:00 +0300

    add: button variant for social login

---
+++ src/components/button/Button.tsx
@@ -0,0 +1,69 @@
+import { styled } from "@stitches/react";
+
+export const Button = styled("button", {
+  border: "none",
+  color: "Black",
+  padding: "1%",
+  borderRadius: "8px",
+
+  variants: {
+    variant: {
+      google: {
+        width: "100%",
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center",
+        fontSize: "large",
+        color: "Black",
+        backgroundColor: "White",
+        boxShadow: "0px 8px 20px rgba(96, 28, 238, 0.5)",
+        "&:hover": {
+          backgroundColor: "GhostWhite",
+        },
+      },
+      twitter: {
+        width: "100%",
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center",
+        fontSize: "large",
+        color: "White",
+        backgroundColor: "#1E8EEE",
+        boxShadow: "0px 8px 20px rgba(96, 28, 238, 0.5)",
+        "&:hover": {
+          backgroundColor: "#187DD3",
+        },
+      },
+      facebook: {
+        width: "100%",
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center",
+        fontSize: "large",
+        color: "White",
+        backgroundColor: "#1877F2",
+        boxShadow: "0px 8px 20px rgba(96, 28, 238, 0.5)",
+        "&:hover": {
+          backgroundColor: "#126BDF",
+        },
+      },
+      apple: {
+        width: "100%",
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center",
+        fontSize: "large",
+        color: "White",
+        backgroundColor: "Black",
+        boxShadow: "0px 8px 20px rgba(96, 28, 238, 0.5)",
+        "&:hover": {
+          backgroundColor: "#212121",
+        },
+      },
+    },
+  },
+});
