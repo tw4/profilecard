3dbe9883f642b73e9a02c4f5bb18e4525472ea8a
mertsabinov <88425310+mertsabinov@users.noreply.github.com>
2023-01-13 17:43:00 +0300

    add: new route user profile

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import globalStyles from "./components/globalStyles";
 import Login from "./pages/Login";
 import Profile from "./pages/Profile";
+import UserProfile from "./pages/UserProfile";
 
 const App = () => {
   globalStyles();
@@ -9,6 +10,7 @@ const App = () => {
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/profile" element={<Profile />} />
+      <Route path="/:user" element={<UserProfile />} />
     </Routes>
   );
 };
