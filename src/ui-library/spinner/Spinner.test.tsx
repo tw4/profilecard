74b043b850df9a116e3dcefeeb65d366e3eebca1
mertsabinov <88425310+mertsabinov@users.noreply.github.com>
2023-02-16 11:27:54 +0300

    add: Spinner test

---
+++ src/ui-library/spinner/Spinner.test.tsx
@@ -0,0 +1,11 @@
+import { screen, render } from '@testing-library/react';
+import { Spinner } from './Spinner';
+
+beforeAll(() => {
+  render(<Spinner data-testid="spin" />);
+});
+
+test('<Spinner/>', () => {
+  const link = screen.findAllByTestId('spin');
+  expect(link).toBeInTheDocument();
+});
