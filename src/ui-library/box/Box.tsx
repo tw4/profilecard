2c7e1ee9a695262b3d8c3e016c606f619a20bf0a
mertsabinov <88425310+mertsabinov@users.noreply.github.com>
2023-02-20 14:40:08 +0300

    add: new variant grid for box

--- src/ui-library/box/Box.tsx
+++ src/ui-library/box/Box.tsx
@@ -1,23 +1,29 @@
-import { styled } from "@stitches/react";
+import { styled } from '@stitches/react';
 
-export const Box = styled("div", {
+export const Box = styled('div', {
   variants: {
     stack: {
       VStack: {
-        height: "100%",
-        width: "100%",
-        display: "flex",
-        flexDirection: "column",
-        textAlign: "center",
+        height: '100%',
+        width: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+        textAlign: 'center',
       },
       HStack: {
-        height: "100%",
-        width: "100%",
-        display: "flex",
-        flexDirection: "row",
-        "@media screen and (max-width: 768px)": {
-          flexDirection: "column",
+        height: '100%',
+        width: '100%',
+        display: 'flex',
+        flexDirection: 'row',
+        '@media screen and (max-width: 768px)': {
+          flexDirection: 'column',
         },
+      },
+      Grid: {
+        height: '100%',
+        width: '100%',
+        display: 'grid',
+        gridTemplateColumns: 'repeat(auto-fit, minmax(400px, 1fr))',
       },
     },
   },
