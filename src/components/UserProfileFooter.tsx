476c38f81e7f3cfc3d540d17d61273ae582a33ce
mertsabinov <88425310+mertsabinov@users.noreply.github.com>
2023-02-20 14:51:21 +0300

    add: user profile footer component

---
+++ src/components/UserProfileFooter.tsx
@@ -0,0 +1,29 @@
+import { Box, Text, Link } from '../ui-library';
+
+const UserProfileFooter = () => {
+  return (
+    <Box
+      stack="HStack"
+      css={{
+        position: 'fixed',
+        left: '0',
+        bottom: '0',
+        height: '5vh',
+        backgroundColor: '#000000',
+        padding: '1%',
+        justifyContent: 'center',
+        '@media screen and (max-width: 768px)': {
+          flexDirection: 'row',
+          marginTop: '5%',
+        },
+      }}
+    >
+      <Text color="light">create your own page :</Text>
+      <Link color="light" href="http://www.profilecard.co/">
+        profilecard.co
+      </Link>
+    </Box>
+  );
+};
+
+export default UserProfileFooter;
