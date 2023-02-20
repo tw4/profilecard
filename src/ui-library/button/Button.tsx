75f0c033cf32ae09c37e03796c2b2019a2dd0ab7
mertsabinov <88425310+mertsabinov@users.noreply.github.com>
2023-02-20 14:45:54 +0300

    add: new variants color for button

--- src/ui-library/button/Button.tsx
+++ src/ui-library/button/Button.tsx
@@ -1,38 +1,43 @@
-import { styled } from "@stitches/react";
+import { styled } from '@stitches/react';
 
-export const Button = styled("button", {
-  width: "100%",
-  borderWidth: "2px",
-  borderColor: "#302C3F",
-  borderStyle: "solid",
-  color: "White",
-  backgroundColor: "#302C3F",
-  borderRadius: "50px",
-  cursor: "pointer",
-  padding: "1.5%",
+export const Button = styled('button', {
+  width: '100%',
+  borderWidth: '2px',
+  borderColor: '#302C3F',
+  borderStyle: 'solid',
+  color: 'White',
+  backgroundColor: '#302C3F',
+  borderRadius: '50px',
+  cursor: 'pointer',
+  padding: '1.5%',
 
   variants: {
     variant: {
       gradient: {
-        backgroundColor: "#1D1A27",
+        backgroundColor: '#1D1A27',
       },
       delete: {
-        color: "#F67280",
+        color: '#F67280',
+      },
+    },
+    color: {
+      purple: {
+        color: '#7353E5',
       },
     },
 
     size: {
-      "1": {
-        fontSize: "xxx-large",
+      '1': {
+        fontSize: 'xxx-large',
       },
-      "2": {
-        fontSize: "xx-large",
+      '2': {
+        fontSize: 'xx-large',
       },
-      "3": {
-        fontSize: "x-large",
+      '3': {
+        fontSize: 'x-large',
       },
-      "4": {
-        fontSize: "large",
+      '4': {
+        fontSize: 'large',
       },
     },
   },
