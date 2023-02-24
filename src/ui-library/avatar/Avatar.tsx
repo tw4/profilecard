9d0eb23fa42ce53555e51d8558dad21941065861
mertsabinov <88425310+mertsabinov@users.noreply.github.com>
2023-02-24 22:03:31 +0300

    update: UI Avatar

--- src/ui-library/avatar/Avatar.tsx
+++ src/ui-library/avatar/Avatar.tsx
@@ -1,20 +1,19 @@
-import { styled } from "@stitches/react";
+import { styled } from '@stitches/react';
 
-export const Avatar = styled("img", {
-  height: "30px",
-  width: "30px",
-  borderRadius: "100%",
-  filter: "drop-shadow(0px 8px 18px rgba(96, 28, 238, 1));",
+export const Avatar = styled('img', {
+  height: '30px',
+  width: '30px',
+  borderRadius: '100%',
+  filter: 'drop-shadow(0px 8px 18px rgba(96, 28, 238, 1));',
 
   variants: {
     variant: {
       profileCard: {
-        height: "100px",
-        width: "100px",
-        marginTop: "5%",
-        filter: "none",
-        borderWidth: "2px",
-        borderStyle: "solid",
+        height: '100px',
+        width: '100px',
+        filter: 'none',
+        borderWidth: '2px',
+        borderStyle: 'solid',
       },
     },
   },
