72e7bef1cd8f78e44c7da37259e5ee59451cdc03
mertsabinov <88425310+mertsabinov@users.noreply.github.com>
2023-02-26 14:03:49 +0300

    add: new navbar item

--- src/components/LandingPage/Navbar.tsx
+++ src/components/LandingPage/Navbar.tsx
@@ -1,22 +1,46 @@
-import { Box, Text } from '../../ui-library';
+import { Box, Text, Link } from '../../ui-library';
 import profileCard from '../../assets/logos/profilecard.svg';
+import NavbarItem from './NavbarItem';
 
 const Navbar = () => {
   return (
     <Box
-      stack="HStack"
+      stack="VStack"
       css={{
         alignItems: 'center',
-        height: '5vh',
-        '@media screen and (max-width: 768px)': {
-          flexDirection: 'row',
-        },
+        justifyContent: 'center',
       }}
     >
-      <img src={profileCard} height="30px" alt="profilecard logo" />
-      <Text color="light" size="4" css={{ fontWeight: 'bold' }}>
-        Profile Card
-      </Text>
+      <Box
+        stack="HStack"
+        css={{
+          alignItems: 'center',
+          justifyContent: 'center',
+          '@media screen and (max-width: 768px)': {
+            flexDirection: 'row',
+          },
+        }}
+      >
+        <img src={profileCard} height="30px" alt="profilecard logo" />
+        <Text color="light" size="4" css={{ fontWeight: 'bold' }}>
+          Profile Card
+        </Text>
+      </Box>
+      <Box
+        stack="HStack"
+        css={{
+          width: '25%',
+          justifyContent: 'space-around',
+          '@media screen and (max-width: 768px)': {
+            flexDirection: 'row',
+            width: '100%',
+          },
+        }}
+      >
+        <NavbarItem href="/" content="Home" />
+        <NavbarItem href="/about" content="About" />
+        <NavbarItem href="https://faq.profilecard.co/" content="Faq" />
+      </Box>
     </Box>
   );
 };
