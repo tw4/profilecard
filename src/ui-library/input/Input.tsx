c8158dab97b1715f7d814ac4851d886e048c7f57
mertsabinov <88425310+mertsabinov@users.noreply.github.com>
2023-03-02 13:57:04 +0300

    update: input

--- src/ui-library/input/Input.tsx
+++ src/ui-library/input/Input.tsx
@@ -1,9 +1,9 @@
-import { styled } from "@stitches/react";
+import { styled } from '@stitches/react';
 
-export const Input = styled("input", {
-  border: "none",
-  borderRadius: "8px",
-  boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25);",
-  padding: "2%",
-  background: "linear-gradient(to right, #CE4DA4, #7353E5)",
+export const Input = styled('input', {
+  border: 'none',
+  borderRadius: '8px',
+  boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25);',
+  padding: '2%',
+  background: 'linear-gradient(to right, $pink, $purple)',
 });
