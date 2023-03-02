7daf5c53ee649c8c0cc07eddc4f963e8e2e8d5f1
mertsabinov <88425310+mertsabinov@users.noreply.github.com>
2023-03-02 13:56:43 +0300

    update: text

--- src/ui-library/text/Text.tsx
+++ src/ui-library/text/Text.tsx
@@ -1,50 +1,50 @@
-import { styled } from "@stitches/react";
+import { styled } from '@stitches/react';
 
-export const Text = styled("p", {
+export const Text = styled('p', {
   variants: {
     validator: {
       error: {
-        color: "Red",
+        color: 'Red',
       },
     },
     color: {
       light: {
-        color: "white",
+        color: 'white',
       },
       grey: {
-        color: "Grey",
+        color: 'Grey',
       },
       reinbow: {
-        background: "linear-gradient(to right, #CE4DA4, #7353E5)",
-        backgroundClip: "text",
-        "-webkit-background-clip": "text",
-        "-webkit-text-fill-color": "transparent",
-        color: "transparent",
-        " -moz-background-clip": "text",
-        " -moz-text-fill-color": "transparent",
+        background: 'linear-gradient(to right, $pink, $purple)',
+        backgroundClip: 'text',
+        '-webkit-background-clip': 'text',
+        '-webkit-text-fill-color': 'transparent',
+        color: 'transparent',
+        ' -moz-background-clip': 'text',
+        ' -moz-text-fill-color': 'transparent',
       },
     },
     size: {
-      "1": {
-        fontSize: "xxx-large",
+      '1': {
+        fontSize: 'xxx-large',
       },
-      "2": {
-        fontSize: "xx-large",
+      '2': {
+        fontSize: 'xx-large',
       },
-      "3": {
-        fontSize: "x-large",
+      '3': {
+        fontSize: 'x-large',
       },
-      "4": {
-        fontSize: "large",
+      '4': {
+        fontSize: 'large',
       },
-      "5": {
-        fontSize: "small",
+      '5': {
+        fontSize: 'small',
       },
-      "6": {
-        fontSize: "x-small",
+      '6': {
+        fontSize: 'x-small',
       },
-      "7": {
-        fontSize: "xx-small",
+      '7': {
+        fontSize: 'xx-small',
       },
     },
   },
