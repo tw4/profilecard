3705a0029f228365a7917bb5e9f648dc9f5c1b5a
mertsabinov <88425310+mertsabinov@users.noreply.github.com>
2023-03-06 12:42:04 +0300

    add: text area

---
+++ src/ui-library/textArea/TextArea.tsx
@@ -0,0 +1,9 @@
+import { styled } from '@stitches/react';
+
+export const TextArea = styled('textarea', {
+  border: 'none',
+  borderRadius: '8px',
+  boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25);',
+  padding: '2%',
+  background: 'linear-gradient(to right, $pink, $purple)',
+});
